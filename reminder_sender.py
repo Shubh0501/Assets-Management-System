90c6bf8465a3045aef2e6a3b5f3e7e39f292bb4c
winterfell <shubham050197@gmail.com>
2018-06-21 14:11:18 +0530

    #Reminder_working

--- reminder_sender.py
+++ reminder_sender.py
@@ -17,13 +17,22 @@ def send_reminder():
         owner = assign[i].owner
         code = assign[i].code
         date = assign[i].next_date
+
+        year = int(date[0:4])
+        month = int(date[5:7])
+        day = int(date[8:])
+        entered_date = datetime.date(year, month, day)
+        date = entered_date
+        reminder = int(assign[i].reminder)
         task = assign[i].task
         loc = assign[i].loc
         sub_loc = assign[i].sub_loc
         section = assign[i].section
         inform_to = assign[i].inform_to
+        reminder = datetime.timedelta(days = reminder)
+        date = date - reminder
 
-        if(str(datetime.datetime.now().date()) == date):
+        if(datetime.datetime.now().date() == date):
             sender = 'vegeta.pasari@gmail.com'
             email = auth.User.objects.get(name=person)
             email = email.mail
@@ -34,7 +43,7 @@ def send_reminder():
             message['Subject'] = 'Job Assignment in '+ code
             message['From'] = sender
             message['To'] = receiver
-            html = 'You have assigned the job for the maintenance of '+ equip + ' on '+ date + '.'+ ' The equipment is owned by '+ owner + ' and the location of equipment is '+ sub_loc+ ', '+loc+ '.'+' The equipment code is '+ code +' and the task is '+task+'. After the job you will inform to '+inform_to
+            html = 'You have assigned the job for the maintenance of '+ equip + ' on '+ str(date) + '.'+ ' The equipment is owned by '+ owner + ' and the location of equipment is '+ sub_loc+ ', '+loc+ '.'+' The equipment code is '+ code +' and the task is '+task+'. After the job you will inform to '+inform_to
             body = MIMEText(html, 'html')
             message.attach(body)
 
@@ -43,6 +52,7 @@ def send_reminder():
             server.login(sender, 'JonSnow5')
             server.sendmail(sender, receiver, message.as_string().encode('utf-8'))
             server.quit()
+            os.system('notify-send "Reminder Sent"')
 
 
 if __name__ == '__main__':
